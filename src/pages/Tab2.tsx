7e29754791a8911a295e5e7613077e0142721dc4
Sarav-Netz <saravjeetsingh.malik@gmail.com>
2022-08-29 22:25:02 +0530

    ionic started

---
+++ src/pages/Tab2.tsx
@@ -0,0 +1,60 @@
+import { 
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+  IonActionSheet,
+  } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Tab2.css';
+import { usePhotoGallery } from '../hooks/usePhotoGallery';
+
+const Tab2: React.FC = () => {
+  const { photos, takePhoto } = usePhotoGallery();
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Photo Galary</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonGrid>
+          <IonRow>
+            {
+              photos.map((photo, index) => {
+                <IonCol size='6' key={index}>
+                  <IonImg src='{photo.webviewPath}' />
+
+                </IonCol>
+              })
+            }
+          </IonRow>
+        </IonGrid>
+        {/* <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Photo Galary</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer name="Tab 2 page" /> */}
+
+        <IonFab vertical='bottom' horizontal='center' slot='fixed'>
+          <IonFabButton onClick={() => takePhoto()}>
+            <IonIcon icon='{camera}'></IonIcon>
+          </IonFabButton>
+        </IonFab>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab2;
